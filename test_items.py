b686c311fed4f00694fbe8eb1d2bf3ecf34d9fe7
shargerdima <36707042+shargerdima@users.noreply.github.com>
2021-05-19 17:56:30 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
