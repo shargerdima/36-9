ad908b3c60c44f9893c6f48eac77d4cc164bffa7
shargerdima <36707042+shargerdima@users.noreply.github.com>
2021-05-19 18:03:49 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,27 +2,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-browser = webdriver.Chrome(options=options)
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Выберите язык: ru, eng, fr или es")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    print("\nЗапуск chrome браузера для тестирования..")
     yield browser
-    print("\nquit browser..")
+    print("\nЗакрытие браузера..")
     browser.quit()
